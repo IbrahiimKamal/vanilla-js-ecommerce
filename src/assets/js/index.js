9d73d031bf1d96d32475c4c303f87b452d157930
kamalo <geeksacademy225@gmail.com>
2020-12-02 15:55:14 +0200

    added banner section and installed AOS and added animation to banner

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,6 @@
 // ##### import external libraries
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,3 +9,6 @@ import '../scss/styles.scss';
 
 // ##### Global Imports #####
 import './shared/toggleNavbar';
+
+// init AOS (animation) library
+AOS.init();
