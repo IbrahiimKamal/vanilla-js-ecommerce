b5d0a190acd154530f4d84c513de2e684c675eeb
kamalo <geeksacademy225@gmail.com>
2020-12-02 14:00:03 +0200

    installed bootstrap and font awesome

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,2 +1,7 @@
+// ##### import external libraries
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// ##### import styles from scss
 import '../scss/styles.scss';
 console.log(`it's working`);
--- src/assets/js/pages/products.js
+++ src/assets/js/pages/products.js
@@ -1,1 +1,8 @@
+// ##### import external libraries
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// ##### import styles from scss
+import '../../scss/styles.scss';
+
 console.log('products page');
