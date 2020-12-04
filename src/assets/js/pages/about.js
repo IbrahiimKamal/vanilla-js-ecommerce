a6c39f967e036e2735bc65dac42da33067db631a
kamalo <geeksacademy225@gmail.com>
2020-12-04 11:14:45 +0200

    added singup page and about page

---
+++ src/assets/js/pages/about.js
@@ -0,0 +1,15 @@
+// ##### import external libraries
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// ##### import styles from scss
+import '../../scss/styles.scss';
+
+// ##### Global Imports #####
+import '../shared/toggleNavbar';
+import '../shared/stickyNavbar';
+
+// init AOS (animation) library
+AOS.init();
